a715d32743796c56031b4e99d2d46c64706b59cd
Claudio Cortese <claudio.cortese@>
2024-10-29 23:42:09 +0100

    Various improvements

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
     AOS.init();
+
+    const featureButtons = document.querySelectorAll('[data-scroll-to]');
+    const scollToElements = {};
+
+    featureButtons.forEach((featureButton) => {
+        scollToElements[`${featureButton.dataset.scrollTo}`] = document.querySelector(featureButton.dataset.scrollTo);
+
+        featureButton.addEventListener('click', ({
+            currentTarget,
+        }) => {
+            if (currentTarget) {
+
+                document.querySelectorAll('.nav-item').forEach((navItem) => {
+                    if (navItem.dataset.scrollTo === currentTarget.dataset.scrollTo) {
+                        navItem.classList.add('active');
+                    } else {
+                        navItem.classList.remove('active');
+                    }
+                });
+
+                scollToElements[`${featureButton.dataset.scrollTo}`]?.scrollIntoView({ behavior: 'smooth', block: 'center' });
+            }
+        })
+    });
 })
