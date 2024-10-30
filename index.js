0fae4ec961ed3a6220cf50e9dca50e6efd23e0b7
Claudio Cortese <claudio.cortese@>
2024-10-30 18:13:49 +0100

    Various improvements

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const featureButtons = document.querySelectorAll('[data-scroll-to]');
     const scollToElements = {};
+    // const btn = document.getElementById('contactFormBtnOpener');
 
     featureButtons.forEach((featureButton) => {
         scollToElements[`${featureButton.dataset.scrollTo}`] = document.querySelector(featureButton.dataset.scrollTo);
@@ -23,5 +24,14 @@ document.addEventListener('DOMContentLoaded', () => {
                 scollToElements[`${featureButton.dataset.scrollTo}`]?.scrollIntoView({ behavior: 'smooth', block: 'center' });
             }
         })
+    });
+
+    document.addEventListener('aos:in:contact-form-button', ({ detail }) => {
+        detail.classList.add('hvr-ripple-out', 'hvr-shadow', 'hvr-icon-forward');
+    });
+
+    document.addEventListener('aos:in:discover-more-button', ({ detail }) => {
+        console.log(detail);
+        detail.classList.add('hvr-ripple-out', 'hvr-shadow', 'hvr-icon-forward');
     });
 })
